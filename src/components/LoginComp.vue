d625ab88c4d3370e06843be52cf1a4545d90b936
KEYs-195 <skatekeys195@gmail.com>
2023-05-24 08:39:40 +0800

    LoginForm
    登录组件根据验证是否发起请求并弹窗提示

--- src/components/LoginComp.vue
+++ src/components/LoginComp.vue
@@ -14,7 +14,7 @@
                         type="password"></el-input>
                 </el-form-item>
                 <el-form-item class="btns">
-                    <el-button type="primary">登录</el-button>
+                    <el-button type="primary" @click=" login ">登录</el-button>
                     <el-button type="info" @click=" resetLoginForm ">重置</el-button>
                 </el-form-item>
             </el-form>
@@ -48,6 +48,15 @@ export default {
         resetLoginForm() {
             // console.log(this);
             this.$refs.loginFormRef.resetFields();
+        },
+
+        login() {
+            this.$refs.loginFormRef.validate(async valid => {
+                if (!valid) return;
+                const { data: res } = await this.$http.post('login', this.loginForm);
+                if (res.meta.status !== 200) return this.$message.error('登陆失败！');
+                this.$message.success('登陆成功！');
+            });
         }
     }
 }
