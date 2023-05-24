9af93800491349d8ef480a23a98f23c270be4e56
KEYs-195 <skatekeys195@gmail.com>
2023-05-24 21:51:43 +0800

    Update HomeComp.vue
    左侧菜单布局

--- src/components/HomeComp.vue
+++ src/components/HomeComp.vue
@@ -10,7 +10,22 @@
         </el-header>
 
         <el-container>
-            <el-aside width="200px">A</el-aside>
+            <el-aside width="200px">
+                <el-menu background-color="#333744" text-color="#fff" active-text-color="#ffd04b">
+                    <el-submenu index="1">
+                        <template slot="title">
+                            <i class="el-icon-location"></i>
+                            <span>导航一</span>
+                        </template>
+                        <el-menu-item index="1-4-1">
+                            <template slot="title">
+                                <i class="el-icon-location"></i>
+                                <span>导航一</span>
+                            </template></el-menu-item>
+                    </el-submenu>
+
+                </el-menu>
+            </el-aside>
 
             <el-main>Main</el-main>
         </el-container>
