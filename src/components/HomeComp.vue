fa723148decf6ba8d118c675ba4c19a5532460ec
KEYs-195 <skatekeys195@gmail.com>
2023-05-24 21:40:37 +0800

    Update HomeComp.vue
    基础布局

--- src/components/HomeComp.vue
+++ src/components/HomeComp.vue
@@ -1,7 +1,15 @@
 <template>
-    <div>
-        <el-button type="info" @click=" ligout ">退出</el-button>
-    </div>
+    <el-container class="home-container">
+        <el-header>
+            <el-button type="info" @click=" ligout ">退出</el-button>
+        </el-header>
+
+        <el-container>
+            <el-aside width="200px">A</el-aside>
+
+            <el-main>Main</el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script>
@@ -15,4 +23,25 @@ export default {
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.home-container {
+    height: 100%;
+}
+
+.el-header {
+    background-color: #373d41;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: #fff;
+    font-size: 20px;
+}
+
+.el-aside {
+    background-color: #333744;
+}
+
+.el-main {
+    background-color: #eaedf1;
+}
+</style>
