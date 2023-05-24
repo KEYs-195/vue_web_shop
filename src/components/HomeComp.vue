97825d7bd5f847966eeda2931f6a9a49c8aa9fb8
KEYs-195 <skatekeys195@gmail.com>
2023-05-24 22:00:15 +0800

    HomeComp
    左侧菜单数据的获取

--- src/components/HomeComp.vue
+++ src/components/HomeComp.vue
@@ -34,11 +34,25 @@
 
 <script>
 export default {
+    data() {
+        return {
+            menulist: []
+        }
+    },
+    created() {
+        this.getMenuList()
+    },
     methods: {
         ligout() {
             window.sessionStorage.clear()
             this.$router.push('/login')
-        }
+        },
+        async getMenuList() {
+            const { data: res } = await this.$http.get('menus')
+            if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+            this.menulist = res.data
+            // console.log(res);
+        },
     }
 }
 </script>
