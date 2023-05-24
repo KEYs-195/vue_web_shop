9371c97da0cb8c8b5c77223bb7b3ade381d977e5
KEYs-195 <skatekeys195@gmail.com>
2023-05-24 20:14:29 +0800

    Update HomeComp.vue
    退出功能的实现

--- src/components/HomeComp.vue
+++ src/components/HomeComp.vue
@@ -1,13 +1,18 @@
 <template>
     <div>
-        home
+        <el-button type="info" @click="ligout">退出</el-button>
     </div>
 </template>
 
 <script>
 export default {
-
+    methods: {
+        ligout() {
+            window.sessionStorage.clear();
+            this.$router.push('/login');
+        }
+    }
 }
 </script>
 
-<style></style>
+<style lang="less" scoped></style>
