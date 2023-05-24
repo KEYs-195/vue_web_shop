17825667731d4ea23c7369224803c0514974eda0
KEYs-195 <skatekeys195@gmail.com>
2023-05-24 08:54:33 +0800

    Update index.js
    路由导航守卫控制访问权限

--- src/router/index.js
+++ src/router/index.js
@@ -24,4 +24,17 @@ const router = new VueRouter({
   routes
 })
 
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 代表从哪个路径跳转来
+  // next 是一个函数 表示放行
+  // next() 放行   next('/login') 强制跳转
+
+  if (to.path === '/login') return next()
+  // 获取 token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
+
 export default router
